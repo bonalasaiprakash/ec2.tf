845243456d6ab70ce3d0476f8d5e1d978d72de71
bonalasaiprakash <107921112+bonalasaiprakash@users.noreply.github.com>
2023-03-08 13:13:15 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "KIA35MGAHN6EQUB5CM5"
-  secret_key = "N+IlcIDSyyKtK6uY8dqcFL7lKs5XLEpQ1RBAq5XB"
-  }
+
 
   resource "aws_instance" "server" {
   ami           = "ami-0a6b2839d44d781b2"
