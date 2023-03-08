47ccd9652226594c625929ed0e33483ffa380da5
bonalasaiprakash <107921112+bonalasaiprakash@users.noreply.github.com>
2023-03-08 13:15:34 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,4 @@
-
-
-  resource "aws_instance" "server" {
+ resource "aws_instance" "server" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c9cb770dfd5a16ab"
@@ -11,3 +9,6 @@
     Name = "sai"
   }
 }
+
+
+ 
